0c0512dedf01499891983ba45c5bc22acca6dd78
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-05-18 12:23:05 +0545

    change file location and add packages.

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { supabase } from "@/integrations/supabase/client";
+import { supabase } from "@/integrations_supabase/client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ProductCard from "@/components/ProductCard";
-import type { Database } from "@/integrations/supabase/types";
+import type { Database } from "@/integrations_supabase/types";
 
 type Product = Database["public"]["Tables"]["products"]["Row"];
 
